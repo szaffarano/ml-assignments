917b0847aebcc979f4231b39c5753c3479592aee
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2015-11-12 20:28:50 -0300

    Second weeks assignments, 100/00

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -16,18 +16,23 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
+    sums = zeros(1, size(X, 2));
+    for i = 1:m
+        for j = 1:size(X, 2)
+            sums(j) += ((theta' * X(i, :)' - y(i)))* X(i, j);
+        end
+    end
 
-
-
-
-
+    for j = 1:size(X, 2)
+        theta(j) = theta(j) - (alpha/m) * sums(j);
+    end
 
 
     % ============================================================
 
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
-
+    disp(sprintf('theta: %f', J_history(iter)));
 end
 
 end
--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -16,22 +16,22 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
+    sums = zeros(1, size(X, 2));
+    for i = 1:m
+        for j = 1:size(X, 2)
+            sums(j) += ((theta' * X(i, :)' - y(i)))* X(i, j);
+        end
+    end
 
-
-
-
-
-
-
-
-
-
+    for j = 1:size(X, 2)
+        theta(j) = theta(j) - (alpha/m) * sums(j);
+    end
 
     % ============================================================
 
     % Save the cost J in every iteration    
     J_history(iter) = computeCostMulti(X, y, theta);
-
+    %fprintf('Costo: %f\n', J_history(iter));
 end
 
 end
