e1cf63e29e0c930b58cfae3149b0dd8d3efaf890
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2015-11-25 21:26:45 -0300

    Make predictions with neural network function

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -10,6 +10,8 @@ num_labels = size(Theta2, 1);
 % You need to return the following variables correctly 
 p = zeros(size(X, 1), 1);
 
+X = [ones(m, 1) X];
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
 %               your learned neural network. You should set p to a 
@@ -20,14 +22,16 @@ p = zeros(size(X, 1), 1);
 %       information see 'help max'. If your examples are in rows, then, you
 %       can use max(A, [], 2) to obtain the max for each row.
 %
+z2 = Theta1 * X';
+a2 = sigmoid(z2)';
 
+a2 = [ones(size(a2, 1), 1) a2];
 
+z3 = (Theta2 * a2')';
+a3 = sigmoid(z3);
+a3 = a3;
 
-
-
-
-
-
+[dummy, p] = max(a3, [], 2);
 
 % =========================================================================
 
