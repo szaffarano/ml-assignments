a186cc535b0d6e0ae72a1f8441a73ad8af1ce7f2
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2015-11-25 21:24:55 -0300

    Logistic regression cost function exercise

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -36,17 +36,11 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
-
-
-
-
-
-
-
-
+J = (-1 / m) * (log (sigmoid(X * theta))' * y + log (1 - sigmoid(X * theta))' * (1 - y));
+J += (sum((theta(2:length(theta)) .^ 2)) * lambda) / (2 * m);
 
 % =============================================================
-
-grad = grad(:);
+grad = (X' * (sigmoid(X * theta) - y)) / m;
+grad = grad + ((lambda / m) * [0; (theta(2:length(theta)))]);
 
 end
